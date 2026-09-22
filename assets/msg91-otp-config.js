9c496e1d4d62c0920568ec060c86e6b428dfa81f
wbulamaboard-org <wbulamaboard@gmail.com>
2026-09-23 01:43:50 +0530

    Update msg91-otp-config.js

--- assets/msg91-otp-config.js
+++ assets/msg91-otp-config.js
@@ -2,5 +2,5 @@
 // Keep the OTP Widget token private. Do not place your MSG91 account AuthKey here.
 window.WBUB_MSG91 = {
   widgetId: "366976734c6b363830373035",
-  tokenAuth: "PASTE_YOUR_MSG91_OTP_WIDGET_TOKEN_HERE"
+  tokenAuth: "WayejinOTP2026"
 };
